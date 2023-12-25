7c59744aa59c0cd99b3a177ae4bedb19a9c9dcf2
Josh Mackelprang <38264690+Mackelprangj@users.noreply.github.com>
2023-12-25 15:42:55 -0500

    Add files via upload

--- src/caesar.js
+++ src/caesar.js
@@ -1,6 +1,4 @@
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
-
   function caesar(input, shift, encode = true) {
     if(!shift || 25 < Math.abs(shift) || shift == 0) return false;
     if (!encode) shift= shift * -1;
@@ -8,22 +6,24 @@ const caesarModule = (function () {
     let newmessage = [];
     let finalmessage = "";
     let decodedNumber = 0;
+    let currentChar = 0;
     //Turns caesar input message into an array of indivual characters
     for (let i = 0; i < input.length; i++){
       newmessage.push(input.toLowerCase().slice(i, i+1));
     }
     for (let y = 0; y < newmessage.length; y++){
+        decodedNumber = 0;
+        currentChar = newmessage[y].charCodeAt(0);
       //checks if the character is part of standard alphabet
-      if (newmessage[y].charCodeAt(0) >= 100 && newmessage[y].charCodeAt(0) <= 125){ 
+      if (currentChar >= 97 && currentChar <= 122){ 
         //checks if after letter shift if the letter needs to wrap around from beginning or end
-        if (newmessage[y].charCodeAt(0) + shift > 125) {decodedNumber = newmessage[y].charCodeAt(0) - 26 + shift}
-        else if (newmessage[y].charCodeAt(0) + shift < 100) {decodedNumber = newmessage[y].charCodeAt(0) + 26 + shift}
-        else {decodedNumber = newmessage[y].charCodeAt(0) + shift}
+        if (currentChar + shift > 122) {decodedNumber = currentChar - 26 + shift}
+        else if (currentChar + shift < 97) {decodedNumber = currentChar + 26 + shift}
+        else {decodedNumber = currentChar + shift}
       }
       if (decodedNumber != 0) finalmessage += String.fromCharCode(decodedNumber);
       else finalmessage += newmessage[y];
     }
-    console.log(finalmessage);
     return finalmessage;
   }
 
